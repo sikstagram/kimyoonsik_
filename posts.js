8d507bb33559fb063c6fc457030328121d622654
sikstagram <4y7g6mhydy@privaterelay.appleid.com>
2026-08-08 23:17:08 +0900

    Update posts.js

--- posts.js
+++ posts.js
@@ -6,11 +6,13 @@ const posts = [
   },
   {
     items: ["https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2019-09-10.MP4"],
+    thumbnail: "https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/thumb2.jpg", // 미리보기 사진 (없으면 회색)
     caption: "MEMORY of JANUARY",
     date: "2019.09.10"
   },
   {
     items: ["https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2019-08-30.MP4"],
+    thumbnail: "https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/thumb1.jpg",
     caption: "#더콜2#mnet#tvn",
     date: "2019.08.30"
   }
@@ -20,4 +22,7 @@ const profile = {
   username: "kimyoonsik_",
   displayName: "김윤식",
   bio: "인스타 아카이브 사이트입니다.",
+  avatar: "https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/profile.jpg", // 프로필 사진
+  followers: "518만",
+  following: "0"
 };

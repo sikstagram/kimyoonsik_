af99026e960c5106361208e637d1d5479de1b465
sikstagram <4y7g6mhydy@privaterelay.appleid.com>
2026-08-09 01:15:09 +0900

    Update posts.js

--- posts.js
+++ posts.js
@@ -1,5 +1,78 @@
 const posts = [
 {
+    items: ["https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2021-12-25.1.JPEG",
+           "https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2021-12-25.2.JPEG",
+           "https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2021-12-25.3.JPEG"],
+    caption: "국을 주미😆",
+    date: "2021.12.25"
+  },
+{
+    items: ["https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2021-12-2.JPEG"],
+    caption: "오늘도 9시 볼륨을높여요🔥
+        #볼륨을높여요 #kbs",
+    date: "2021.12.02"
+  },
+    {
+    items: ["https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2021-11-26.MP4"],
+    caption: "댄싱머신",
+    date: "2021.11.26"
+  },
+    {
+    items: ["https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2021-11-12.1.MP4",
+           "https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2021-11-12.2.JPEG"],
+    caption: "U+디즈니👍
+        #디즈니",
+    date: "2021.11.12"
+  },
+    {
+    items: ["https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2021-10-14.1.JPEG",
+           "https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2021-10-14.2.JPEG"],
+    date: "2021.10.14"
+  },
+    {
+    items: ["https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2021-08-26.1.MP4",
+           "https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2021-08-26.2.MP4"],
+    caption: "👋
+        #슬기로운의사생활시즌2",
+    date: "2021.08.26"
+  },
+    {
+    items: ["https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2021-09-04.1.JPEG",
+           "https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2021-09-04.2.JPEG"],
+    caption: "머리를 많이 길렀다",
+    date: "2021.08.04"
+  },
+    {
+    items: ["https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2021-07-08.1.JPEG"],
+    caption: "VOGUE",
+    date: "2021.07.08"
+  },
+    {
+    items: ["https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2021-07-16.MP4"],
+    caption: "교감중
+        
+        일방적인",
+    date: "2021.07.16"
+  },
+    {
+    items: ["https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2021-07-14.JPEG"],
+    date: "2021.07.14"
+  },
+    {
+    items: ["https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2021-07-08.1.JPEG",
+           "https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2021-07-08.2.JPEG"],
+    caption: "안녕하세요 윤도건입니다😊
+        
+        약 한달간 정말 많은 분들께서 카페와 회사를 통해 많은 선물과 편지를 전해 주셨습니다..❤️
+    
+정말 과분한 사랑에 감사하기도하고 죄송한 마음도 큰거같습니다^^
+    
+    한분한분 선물 인증을 해드리고싶었지만..(흑흑) 그러지 못한점은 정말 죄송합니다😭 하지만 정성들여 준비하신 선물과, 편지, 마음까지(❤️) 깊이 감사하고있습니다><
+    
+    더 좋은 모습보여드릴수있는 배우 되도록 노력하겠습니다! 감사합니다❤️",
+    date: "2021.07.08"
+  },
+    {
     items: ["https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/2021-01-15.JPEG"],
     date: "2021.01.15"
   },

ba67fee8d32364431e7141c35a62161e1eaf7a93
sikstagram <4y7g6mhydy@privaterelay.appleid.com>
2026-08-08 21:45:46 +0900

    Update posts.js

--- posts.js
+++ posts.js
@@ -1,16 +1,16 @@
 const posts = [
   {
     items: ["https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/kimyoonsik_2019-08-30.MP4"],
-    caption: "#더콜2#mnet#tvn"
+    caption: "#더콜2#mnet#tvn",
     date: "2019.08.30"
   },
   {
     items: ["https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/kimyoonsik_2019-09-10.MP4"],
-    caption: "MEMORY of JANUARY"
+    caption: "MEMORY of JANUARY",
     date: "2019.09.10"
-  }
- {
-    items: ["kimyoonsik_2019-09-17.JPGE"],
+  },
+  {
+    items: ["kimyoonsik_2019-09-17.JPEG"],
     caption: "#vogue",
     date: "2019.09.17"
   }

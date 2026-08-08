872255bb012a2adb9b579dcdb56c2bb4c4496a26
sikstagram <4y7g6mhydy@privaterelay.appleid.com>
2026-08-08 23:50:37 +0900

    Update posts.js

--- posts.js
+++ posts.js
@@ -23,6 +23,6 @@ const profile = {
   displayName: "김윤식",
   bio: "인스타 아카이브 사이트입니다.",
   avatar: "https://raw.githubusercontent.com/sikstagram/kimyoonsik_/main/profile.jpg.jpg",
-  followers: "1.2만",
-  following: "320"
+  followers: "518만",
+  following: "0"
 };
